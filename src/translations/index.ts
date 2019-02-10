68741deb579f767339a37866ff21f30cd3d29cb8
Sandro Roth <>
2019-02-10 11:56:27 +0100

    Improve locales object

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,16 +2,31 @@ import Vue from 'vue';
 import moment from 'moment';
 import VueI18n from 'vue-i18n';
 import { english } from './english';
-import { Localization } from './types';
+import { Localization, Locale } from './types';
 import { initLanguage } from '@/core/localizer';
 import { german } from './german';
 
 Vue.use(VueI18n);
 
-export const messages: { [key: string]: Localization } = {
-  en: english,
-  de: german,
-};
+export const locales: Locale[] = [
+  {
+    key: 'en',
+    name: 'English',
+    messages: english,
+  },
+  {
+    key: 'de',
+    name: 'Deutsch',
+    messages: german,
+  },
+];
+
+export const messages: { [key: string]: Localization } = locales.reduce((obj, locale) => {
+  return {
+    ...obj,
+    [locale.key]: locale.messages,
+  };
+}, {});
 
 export const i18n = new VueI18n({
   locale: 'en',
@@ -20,8 +35,8 @@ export const i18n = new VueI18n({
 });
 
 // Init moment.js
-Object.entries(messages).forEach(([key, message]) => {
-  moment.locale(key, message.TIME);
+locales.forEach((locale) => {
+  moment.locale(locale.key, locale.messages.TIME);
 });
 
 // Init language
--- src/translations/types.ts
+++ src/translations/types.ts
@@ -1,5 +1,11 @@
 import VueI18n from 'vue-i18n';
 
+export interface Locale {
+  key: string;
+  name: string;
+  messages: Localization;
+}
+
 export interface Localization extends VueI18n.LocaleMessages {
   TIME: {
     [name: string]: any;
